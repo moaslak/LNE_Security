6cc61722944eabdd6ed818e5f82eb9968b1a05e7
stef663k <stef663k@elev.techcollege.dk>
2022-03-18 12:35:53 +0100

    D1

--- LNE_Security/LNE_Security/Database.cs
+++ LNE_Security/LNE_Security/Database.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LNE_Security
+namespace LNE_Security;
+
+partial class Database
 {
-    partial class Database
-    {
-    }
+    public int ID { get; set; }
+    public string CompanyName { get; set; }
+    public string RoadName { get; set; }
+    public int HouseNumber { get; set; }
+    public int ZipCode { get; set; }
+    public string Country { get; set; }
+    public Decimal Currency { get; set; }
+
 }
