bf5c5f3561ca1027d66ddb5c41ced41128645ca6
stef663k <stef663k@elev.techcollege.dk>
2022-03-22 14:00:08 +0100

    D2

--- LNE_Security/LNE_Security/Company/Product.cs
+++ LNE_Security/LNE_Security/Company/Product.cs
@@ -5,8 +5,22 @@ using System.Text;
 
 namespace LNE_Security
 {
+    public enum UnitPrice
+    {
+        Hour,
+        Meter
+    }
     public class Product
     {
+        public int ProductNumber { get; set; }
+        public string ProductName 
+        {
+            get { return ProductName; }
+            set { ProductName = value; }
+        }
+        public float SalesPrice { get; set; }
+        public float CompanyPrice { get; set; }
+        public float AmountInStorage { get; set; }
         public UInt32 ID
         {
             get => default;
@@ -23,12 +37,16 @@ namespace LNE_Security
             }
         }
 
-        public double PricePerItem
+        public void CalculateProfit()
         {
-            get => default;
-            set
-            {
-            }
+            Console.Write($"Indtast:{UnitPrice.Hour} {UnitPrice.Meter}");
+            var unitPrice = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Vælg mængden: ");
+            var amount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(unitPrice * amount);
+            var percent = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine((percent / SalesPrice) * 100);
         }
+
     }
 }
