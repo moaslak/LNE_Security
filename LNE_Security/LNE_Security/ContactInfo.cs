c3a0040f47b9878fa574f43cc6b95fe0784547bf
stef663k <stef663k@elev.techcollege.dk>
2022-03-22 09:59:00 +0100

    ContachInfo v.1

--- LNE_Security/LNE_Security/ContactInfo.cs
+++ LNE_Security/LNE_Security/ContactInfo.cs
@@ -7,5 +7,10 @@ namespace LNE_Security
 {
     public class ContactInfo
     {
+        public int ID { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
     }
 }
