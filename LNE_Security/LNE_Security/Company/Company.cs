0212200d404c3c2ebce0d2d66faaafa5b8a71e47
moaslak <moaslak@gmail.com>
2022-03-22 12:46:26 +0100

    company class updated

--- LNE_Security/LNE_Security/Company/Company.cs
+++ LNE_Security/LNE_Security/Company/Company.cs
@@ -30,5 +30,69 @@ namespace LNE_Security
             {
             }
         }
+
+        public string CVR
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string CompanyName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string StreetName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string HouseNumber
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string ZipCode
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string City
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Country
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Currency
+        {
+            get => default;
+            set
+            {
+            }
+        }
     }
 }
