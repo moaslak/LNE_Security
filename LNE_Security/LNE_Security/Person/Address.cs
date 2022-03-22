7292864be830b8040dbd30386deccaa54bfdd85a
stef663k <stef663k@elev.techcollege.dk>
2022-03-22 10:29:39 +0100

    Address v.1

--- LNE_Security/LNE_Security/Person/Address.cs
+++ LNE_Security/LNE_Security/Person/Address.cs
@@ -7,5 +7,10 @@ namespace LNE_Security
 {
     public class Address
     {
+        public UInt16 ID { get; set; }
+        public string StreetName { get; set; }
+        public string HouseNumber { get; set; }
+        public UInt16 ZipCode { get; set; }
+        public string Country { get; set; }
     }
 }
