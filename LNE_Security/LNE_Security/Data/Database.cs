2f466b6b1a9bd125f2068c6cb15ce921262867b5
stef663k <stef663k@elev.techcollege.dk>
2022-03-24 12:23:16 +0100

    P1

--- LNE_Security/LNE_Security/Data/Database.cs
+++ LNE_Security/LNE_Security/Data/Database.cs
@@ -5,11 +5,41 @@ using System.Text;
 
 namespace LNE_Security
 {
-    public class Database
+    public class Database : Product
     {
         // TODO: implementér singleton
         public static Company Instance { get; private set; }
+        Product product = new Product();
+        List<Product> products = new List<Product>();
+        List<Database> databases = new List<Database>();
 
+        public Database(uint id, string productName)
+        {
+            product.ID = id;
+            product.ProductName = productName;
+            products.ForEach(p => p.ID = id);
+            products.ForEach(p => p.ProductName = productName);
+            
+        }
+
+        public Database Insert()
+        {
+            products.Add(product);
+            return this;
+        }
+        public Database Update()
+        {
+            product = new Product();
+            ID = product.ID;
+            return Insert();
+        }
+
+        public Database Delete()
+        {
+            product.ID--;
+            products.Remove(product);
+            return this;
+        }
         /*static Company()
         {
             Instance = new Company();
