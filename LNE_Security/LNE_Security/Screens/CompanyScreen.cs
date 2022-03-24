a7b198e6c4a144e99bc6169dd520ce0448aca1be
moaslak <moaslak@gmail.com>
2022-03-24 14:21:37 +0100

    V3 - DONE V4 - partial V5 - partial

--- LNE_Security/LNE_Security/Screens/CompanyScreen.cs
+++ LNE_Security/LNE_Security/Screens/CompanyScreen.cs
@@ -27,21 +27,29 @@ namespace LNE_Security
             CompanyListPage.AddColumn("Company name", "CompanyName");
             CompanyListPage.AddColumn("Country", "Country");
             CompanyListPage.AddColumn("Currency", "Currency");
+            Console.WriteLine("Choose company");
             Company selected = CompanyListPage.Select();
             Console.WriteLine("Selection: " + selected.CompanyName);
-            Console.WriteLine("F1 - Details");
-            Console.WriteLine("F2 - New company");
+            Console.WriteLine("F1 - New company");
+            Console.WriteLine("F2 - Edit");
             Console.WriteLine("F10 - To Main menu");
             Console.WriteLine("Esc - Close App");
             switch (Console.ReadKey().Key)
             {
                 case ConsoleKey.F1:
+                    Console.WriteLine("IMPLEMENT NEW COMPANY");
+                    break;
+                case ConsoleKey.F2:
+                    EditCompnayScreen editScreen = new EditCompnayScreen(company);
+                    ScreenHandler.Display(editScreen);
+                    break;
+
+
+                case ConsoleKey.F11:
                     CompanyDetailsScreen details = new CompanyDetailsScreen(selected);
                     ScreenHandler.Display(details);
                     break;
-                case ConsoleKey.F2:
-                    Console.WriteLine("IMPLEMENT NEW COMPANY");
-                    break;
+                
                 case ConsoleKey.F10:
                     MainMenuScreen menu = new MainMenuScreen(company);
                     ScreenHandler.Display(menu);
--- LNE_Security/LNE_Security/Screens/EditCompnayScreen.cs
+++ LNE_Security/LNE_Security/Screens/EditCompnayScreen.cs
@@ -3,10 +3,96 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TECHCOOL.UI;
 
 namespace LNE_Security.Screens
 {
     internal class EditCompnayScreen : ScreenHandler
     {
+        class Options
+        {
+            public string Option { get; set; }
+            public string Value { get; set; }
+            public Options(string option, string value)
+            {
+                Value = value;
+                Option = option;
+            }
+        }
+        private Company company;
+        public EditCompnayScreen(Company Company) : base(Company)
+        {
+            this.company = Company;
+        }
+
+        private void EditCompany(Options selected)
+        {
+            Console.Write("New value: ");
+            string newValue = Console.ReadLine();
+
+            switch (selected.Option)
+            {
+                case "Company name":
+                    this.company.CompanyName = newValue;
+                    break;
+                case "Street name":
+                    this.company.StreetName = newValue;
+                    break;
+                case "House number":
+                    this.company.HouseNumber = newValue;
+                    break;
+                case "Zip code":
+                    this.company.ZipCode = newValue;
+                    break;
+                case "City":
+                    this.company.City = newValue;
+                    break;
+                case "Country": 
+                    this.company.Country = newValue;
+                    break;
+                case "Currency":
+                    break; // TODO: det er en ENUM. LAV DENNE.
+                default:
+                    break;
+            }   
+        }
+
+        protected override void Draw()
+        {
+            do
+            {
+                Title = company.CompanyName + " Edit company screen";
+                Clear(this);
+                ListPage<Company> CompanyListPage = new ListPage<Company>();
+
+                CompanyListPage.AddColumn("Company name", "CompanyName");
+                CompanyListPage.AddColumn("Street name", "StreetName");
+                CompanyListPage.AddColumn("House number", "HouseNumber");
+                CompanyListPage.AddColumn("Zip code", "ZipCode");
+                CompanyListPage.AddColumn("City", "City");
+                CompanyListPage.AddColumn("Country", "Country");
+                CompanyListPage.AddColumn("Currency", "Currency");
+                CompanyListPage.Add(company);
+                CompanyListPage.Draw();
+
+                ListPage<Options> optionsListPage = new ListPage<Options>();
+
+                optionsListPage.AddColumn("Edit", "Option");
+                optionsListPage.Add(new Options("Company name", company.CompanyName));
+                optionsListPage.Add(new Options("Street name", company.StreetName));
+                optionsListPage.Add(new Options("House number", company.HouseNumber));
+                optionsListPage.Add(new Options("Zip code", company.ZipCode));
+                optionsListPage.Add(new Options("City", company.City));
+                optionsListPage.Add(new Options("Country", company.Country));
+                Options selected = optionsListPage.Select();
+                EditCompany(selected);
+                Console.WriteLine("Press a key to update..."); // TODO: Denne skal gerne væk
+                Console.WriteLine("Press ESC to return to Company screen");
+            } while ((Console.ReadKey().Key != ConsoleKey.Escape));
+
+            CompanyScreen companyScreen = new CompanyScreen(company);
+            ScreenHandler.Display(companyScreen);
+            
+        }
     }
 }
