ace41c10b843456373f2ba817fcf80b151b765af
moaslak <moaslak@gmail.com>
2022-05-04 12:38:04 +0200

    password masking

--- LNE_Security/LNE_Security/Screens/EmployeeScreen.cs
+++ LNE_Security/LNE_Security/Screens/EmployeeScreen.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TECHCOOL.UI;
 using LNE_Security;
+using System.Security;
 
 namespace LNE_Security.Screens;
 
@@ -58,17 +59,16 @@ public class EmployeeScreen : ScreenHandler
         contactInfo.AddressId = Database.Instance.NewAddress(contactInfo.Address);
 
         Employee newEmployee = new Employee();
-        Console.WriteLine("Enter user name: ");
+        Console.Write("Enter user name: ");
         newEmployee.UserName = Console.ReadLine();
-        Console.WriteLine("Enter password: ");
-        newEmployee.Password = Console.ReadLine();
-
+        Console.Write("Enter password: ");
+        newEmployee.Password = newEmployee.GetPassword().ToString();
         int passwordCheck = 0;
         bool passwordConfirmed = false;
         do
         {
-            Console.WriteLine("Confirm password");
-            if (Console.ReadLine() == newEmployee.Password)
+            Console.Write("Confirm password: ");
+            if (newEmployee.Password == newEmployee.GetPassword())
                 passwordConfirmed = true;
             else
             {
@@ -91,7 +91,6 @@ public class EmployeeScreen : ScreenHandler
         newEmployee.Address = contactInfo.Address;
         Database.Instance.NewEmployee(newEmployee);
     }
-
     private void viewCustomer(Employee employee)
     {
         Console.Write("Name: " + employee.FullName + "\n");
